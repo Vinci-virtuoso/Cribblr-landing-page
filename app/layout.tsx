7274c5d7b32bfca0b7f74f2d859b052c2de54b0d
Vinci <5eun3isiyktv@gmail.com>
2025-01-20 15:25:34 +0100

    final changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/images/logo.webp" />
+        <link rel="icon" href="/images/C.webp" />
         {/* Other meta tags, link tags, etc. */}
       </head>
       <body>{children}</body>
